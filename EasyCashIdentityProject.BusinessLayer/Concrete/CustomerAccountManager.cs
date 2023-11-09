57fea56df7edc0f22609301a962381ccb2f38fc3
Merve Bayindir <76398913+mervebayindir@users.noreply.github.com>
2023-11-09 23:39:22 +0300

    1. Temalar eklendi 2. Businass katmanına CustomerAccountProcessManager  ve CustomerAccountPManager implemetleriyle birlikte oluşturuldu.

---
+++ EasyCashIdentityProject.BusinessLayer/Concrete/CustomerAccountManager.cs
@@ -0,0 +1,46 @@
+﻿using EasyCashIdentityProject.BusinessLayer.Abstract;
+using EasyCashIdentityProject.DataAccessLayer.Abstract;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.BusinessLayer.Concrete
+{
+    public class CustomerAccountManager : ICustomerAccountService
+    {
+        private readonly ICustomerAccountDal _customerAccountDal;
+
+        public CustomerAccountManager(ICustomerAccountDal customerAccountProcessService)
+        {
+            _customerAccountDal = customerAccountProcessService;
+        }
+
+        public void TDelete(CustomerAccount t)
+        {
+            _customerAccountDal.Delete(t);
+        }
+
+        public List<CustomerAccount> TGetList()
+        {
+            return _customerAccountDal.GetList();
+        }
+
+        public void TInsert(CustomerAccount t)
+        {
+            _customerAccountDal.Insert(t);
+        }
+
+        public void TUpdate(CustomerAccount t)
+        {
+            _customerAccountDal.Update(t);
+        }
+
+        public CustomerAccount TGetByID(int id)
+        {
+            return _customerAccountDal.GetById(id);
+        }
+    }
+}
