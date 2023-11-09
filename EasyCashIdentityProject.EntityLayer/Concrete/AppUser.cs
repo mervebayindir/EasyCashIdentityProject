4be7097a104e6702ed2b92e3e26f18d38de66ca4
Merve Bayindir <76398913+mervebayindir@users.noreply.github.com>
2023-11-09 16:55:49 +0300

    1. Database tabloları oluşturulup düzenlendi. 2.AppUser ve AppRole sınıfları oluşturuldu 3.Context sınıfı oluşturuldu DbSet'ler eklendi

---
+++ EasyCashIdentityProject.EntityLayer/Concrete/AppUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.EntityLayer.Concrete
+{
+    public class AppUser:IdentityUser<int>  //integer bir değer girebilmek için key ekliyoruz primary key type seçiyoruz bizim projemizde gerek olmayabilir normalde string değerdir
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string District { get; set; }
+        public string City { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
