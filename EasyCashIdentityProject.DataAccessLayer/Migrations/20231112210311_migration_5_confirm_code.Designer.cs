b2e4278f7db3dcece3e4f5d23b2aecd6e0c72752
Merve Bayindir <76398913+mervebayindir@users.noreply.github.com>
2023-11-13 19:09:52 +0300

    1. Register işlemi gerçekleştirildi ve sonrasında MailConfirm sayfasına yönlendirme işlemi yapıldı. 2. Maile onay kodu yollanıp girildiğinde eşleşen onay kodu MyProfile  sayfasına yönlendirme işlemi tamamlandı

--- EasyCashIdentityProject.DataAccessLayer/Migrations/20231112210311_migration_5_confirm_code.Designer.cs
+++ EasyCashIdentityProject.DataAccessLayer/Migrations/20231112210311_migration_5_confirm_code.Designer.cs
@@ -4,6 +4,7 @@ using EasyCashIdentityProject.DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EasyCashIdentityProject.DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20231112210311_migration_5_confirm_code")]
+    partial class migration_5_confirm_code
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EasyCashIdentityProject.PresentationLayer/Models/CustomIdentityValidator.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EasyCashIdentityProject.PresentationLayer.Models
+{
+	public class CustomIdentityValidator : IdentityErrorDescriber
+	{
+		public override IdentityError PasswordTooShort(int length)  // şifrenin çok kısa olduğunu bildiren methoddur
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordTooShort",  //Key değeridir ve methodun ismi yazılır.
+				Description = $"Parola en az {length} karakter olmalıdır"
+			};
+		}
+		public override IdentityError PasswordRequiresUpper()
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresUpper",
+				Description = "Lütfen en az 1 büyük harf giriniz"
+			};
+		}
+		public override IdentityError PasswordRequiresLower()
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresLower",
+				Description = "Lütfen en az 1 küçük harf giriniz"
+			};
+		}
+		public override IdentityError PasswordRequiresDigit()
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresDigit",
+				Description = " Lütfen en az 1 tane rakam giriniz"
+			};
+		}
+		public override IdentityError PasswordRequiresNonAlphanumeric()
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresNonAlphanumeric",
+				Description = "Lütfen en az 1 tane sembol giriniz"
+			};
+		}
+		public override IdentityError DuplicateUserName(string userName)
+		{
+			return new IdentityError()
+			{
+				Code = "InvalidToken",
+				Description = "Aynı isimde kullanıcı ismi vardır. Lütfen başka bir kullanıcı ismi giriniz"
+			};
+		}
+	}
+}
