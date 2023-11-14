f9ac5afa46e9327bd3e4c127aab0e55119d2a6dc
Merve Bayindir <76398913+mervebayindir@users.noreply.github.com>
2023-11-14 15:18:10 +0300

    Template düzen yapıldı

---
+++ EasyCashIdentityProject.PresentationLayer/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using EasyCashIdentityProject.EntityLayer.Concrete;
+using EasyCashIdentityProject.PresentationLayer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentityProject.PresentationLayer.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
+
+        public LoginController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginViewModel loginViewModel)
+        {
+            var result = await _signInManager.PasswordSignInAsync(loginViewModel.UserName, loginViewModel.Password, false, true);  //4 parametre girilmesi zorunlu 3.PARAMETRE ==> false değeri kullanıcı tekrar girişinde kullanıcı işmi ve şifre hatırlansın mı? ayarı  4.PARAMETRE true yaparsak beş tane yanlış girilen kullanıcı adı ve şifre olursa sistemde belli bir süre engellenir.
+            if (result.Succeeded)
+            {
+                var user = await _userManager.FindByNameAsync(loginViewModel.UserName);
+                if (user.EmailConfirmed == true)
+                {
+                    return RedirectToAction("Index", "MyProfile");
+                }
+
+            }
+            return View();
+        }
+    }
+}
